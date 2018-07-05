bc7cc7f6102b04d92fb60d8f9b8b0b4b3d02a3d3
Bobby Galvan <bgalvan10@gmail.com>
2018-07-05 16:03:57 -0500

    added stop motion video

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var checkout = require('./checkout');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index');
+  res.render('pages/bootstrap');
 });
-
-
-router.use('/checkout', checkout);
 
 
 module.exports = router;
